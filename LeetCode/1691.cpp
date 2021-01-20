d445f8bfe3124fcf61f0d92d32e825271be99f04
Kona1956 <18481860700>
2021-01-20 14:58:27 +0800

    1961

--- LeetCode/1691.cpp
+++ LeetCode/1691.cpp
@@ -21,33 +21,31 @@ const ll mod = 1000000007;
 ll powmod(ll a, ll b) { ll res = 1; a %= mod; for (; b; b >>= 1) { if (b & 1) res = res * a % mod; a = a * a % mod;}return res;}
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
 
-vector<int> G[105]; 
+int G[105][105]; 
+int size[105]; 
 int height[105]; 
+bool vis[105]; 
+int res; 
 
 void DFS(int i, vector<vector<int>>& cuboids)
 {
-    for(auto j: G[i])
+    int maxh = 0; 
+    vis[i] = true; 
+    for(int j = 0; j < size[i]; j++)
     {
-        DFS(j, cuboids); 
-        height[i] = max(height[i], height[j] + cuboids[j][2]); 
+        if(vis[G[i][j]] == false)
+            DFS(G[i][j], cuboids); 
+        maxh = max(maxh, height[G[i][j]]); 
     }
-}
-
-pair<int, int> cmp(vector<int> A, vector<int> B)
-{
-    pair<int, int> res; res.first = -1; res.second = -1; 
-    if(A[0] >= B[0] && A[1] >= B[1] && A[2] >= B[2]) res.first = 1; 
-    if(A[0] <= B[0] && A[1] <= B[1] && A[2] <= B[2]) res.second = 1; 
-    return res; 
+    res = max(res, height[i] = maxh + cuboids[i][2]); 
 }
 
 int maxHeight(vector<vector<int>>& cuboids) 
 {
     int n = cuboids.size(); 
-    int ind[105]; 
-
+    memset(size, 0, sizeof size); 
     memset(height, 0, sizeof height); 
-    memset(ind, 0, sizeof ind); 
+    memset(vis, 0, sizeof(vis)); 
     for(int i = 0; i < n; i++)
     {
         sort(cuboids[i].begin(), cuboids[i].end());
@@ -56,23 +54,17 @@ int maxHeight(vector<vector<int>>& cuboids)
     {
         for(int j = i + 1; j < n; j++)
         {
-            auto tmp = cmp(cuboids[i], cuboids[j]); 
-            if(tmp.first == 1) G[i].push_back(j), ind[j]++; 
-            if(tmp.second == 1) G[j].push_back(i), ind[i]++; 
+            if(cuboids[i][0] >= cuboids[j][0] && cuboids[i][1] >= cuboids[j][1] && cuboids[i][2] >= cuboids[j][2]) G[i][size[i]++] = j; 
+            if(cuboids[i][0] <= cuboids[j][0] && cuboids[i][1] <= cuboids[j][1] && cuboids[i][2] <= cuboids[j][2]) G[j][size[j]++] = i; 
         }
     }
-    
+    res = -1; 
     for(int i = 0; i < n; i++)
     {
-        if(ind[i] == 0)
+        if(vis[i] == false)
         {
             DFS(i, cuboids); 
         }
-    }
-    int res = -1; 
-    for(int i = 0; i < n; i++)
-    {
-        res = max(res, height[i]);    
     }
     return res; 
 }
@@ -87,18 +79,21 @@ int main()
     freopen("output.txt", "w", stdout);
     #endif
     /* code */
-    int n, x, y, z;
-    cin >> n; 
-    rep(i, 0, n)
-    {
-        cin >> x >> y >> z; 
-        vector<int> V; 
-        V.push_back(x);
-        V.push_back(y);
-        V.push_back(z);
-        VVI.push_back(V); 
-    }
-    cout << VVI.size() << endl; 
+    vector<int> V1{7,11,17};
+    VVI.push_back(V1);  
+    vector<int> V2{7,11,17};
+    VVI.push_back(V2);  
+    vector<int> V3{7,11,17};
+    VVI.push_back(V3);  
+    vector<int> V4{7,11,17};
+    VVI.push_back(V4);  
+    vector<int> V5{7,11,17};
+    VVI.push_back(V5);  
+    vector<int> V6{7,11,17};
+    VVI.push_back(V6);  
+    // vector<int> V3{45, 23, 12};
+    // VVI.push_back(V3);  
+    // cout << VVI.size() << endl; 
     cout << maxHeight(VVI) << endl;
     return 0;
 }
