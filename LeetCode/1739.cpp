7609ed0443c0b20816a40cd7ab46e9a20eac7f79
Kona1956 <18481860700>
2021-02-25 21:50:59 +0800

    leetcode 1739

---
+++ LeetCode/1739.cpp
@@ -0,0 +1,55 @@
+//created by Kona @VSCode
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define LOCAL_TEST
+#define rep(i, a, n) for (int i = a; i<n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int, int> PII;
+typedef vector<pair<int, int>> VPII;
+typedef map<int, int> MII;
+const ll mod = 1000000007;
+ll powmod(ll a, ll b) { ll res = 1; a %= mod; for (; b; b >>= 1) { if (b & 1) res = res * a % mod; a = a * a % mod;}return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+
+class Solution {
+public:
+    int minimumBoxes(int n) {
+        int cnt = 1; 
+        int pre = 0; 
+        int sum = 0; 
+        while(sum + pre + cnt <= n)
+        {
+            sum += pre + cnt; 
+            pre += cnt; 
+            cnt++; 
+        }
+        int res = pre; 
+        int s = sqrt(2 * (n - sum)); 
+        while(s * (s + 1) < 2 * (n - sum)) s++;  
+        return res + s; 
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    #ifdef LOCAL_TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    /* code */
+
+    Solution app; 
+    app.minimumBoxes(51); 
+    return 0;
+}
