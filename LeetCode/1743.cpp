f94e8f108be5daac762ab7e05fb225b07a2f8ab8
K-ona <yt11011997@gmail.com>
2021-03-09 13:13:52 +0800

    leetcode 1743

---
+++ LeetCode/1743.cpp
@@ -0,0 +1,83 @@
+//created by Kona @VSCode
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define LOCAL_TEST
+#define rep(i, a, n) for (int i = a; i<n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int, int> PII;
+typedef vector<pair<int, int>> VPII;
+typedef map<int, int> MII;
+const ll mod = 1000000007;
+ll powmod(ll a, ll b) { ll res = 1; a %= mod; for (; b; b >>= 1) { if (b & 1) res = res * a % mod; a = a * a % mod;}return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+
+class Solution {
+    bool vis[2 * (int)1e5 + 5]; 
+    vector<int> G[2 * (int)1e5 + 5]; 
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        
+        int n = adjacentPairs.size(); 
+        for(int i = 0; i < n; i++)
+        {
+            G[adjacentPairs[i][0] + (int)1e5].push_back(adjacentPairs[i][1] + (int)1e5); 
+            G[adjacentPairs[i][1] + (int)1e5].push_back(adjacentPairs[i][0] + (int)1e5); 
+        }
+
+        int s = 2 * (int)1e5;  
+        int si = -1; 
+        for (int i = 0; i < 2 * (int)1e5 + 5; i++)
+        {
+            if(!G[i].empty())
+            {
+                if(s > G[i].size())
+                {
+                    si = i; 
+                    s = G[i].size(); 
+                }
+            }
+        }
+
+        vector<int> ans; 
+        memset(vis, 0, sizeof vis); 
+        vis[si] = true; 
+        DFS(si, ans); 
+        return ans; 
+    }
+
+    void DFS(int v, vector<int> & ans)
+    {
+        ans.push_back(v - (int)1e5); 
+        for(int i = 0; i < G[v].size(); i++)
+        {
+            if(!vis[G[v][i]])
+            {
+                vis[G[v][i]] = true;
+                DFS(G[v][i], ans);
+            }
+        }
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    #ifdef LOCAL_TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    /* code */
+
+
+    return 0;
+}
