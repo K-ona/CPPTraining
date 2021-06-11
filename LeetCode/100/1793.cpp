e7b4828b922204b5a29a4a7948fb38dafb9e509b
Kona1956 <38185987+K-ona@users.noreply.github.com>
2021-06-11 18:43:27 +0800

    .

---
+++ LeetCode/100/1793.cpp
@@ -0,0 +1,48 @@
+//created by Kona @VSCode
+#include <iostream>
+#include <vector>
+#include <stack>
+
+#define LOCAL_TEST
+typedef long long ll;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int l[100005];
+int r[100005];
+
+class Solution {
+public:
+    int maximumScore(std::vector<int>& nums, int k) {
+        std::stack<int> St;
+        // int St[100005]; 
+        // int top = -1;  
+        for (int i = 0; i < (int)nums.size(); ++i) {
+            if (St.empty() || nums[St.top()] < nums[i]) {
+                St.push(i); 
+            }
+            else {
+                while(!St.empty()) {
+                    r[St.top()] = i; 
+                    St.pop(); 
+                }
+                St.push(i); 
+            }
+        }
+
+
+    }
+};
+
+int main() {
+    std::ios_base::sync_with_stdio(false); 
+    #ifdef LOCAL_TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    /* code */
+
+
+    return 0;
+}
