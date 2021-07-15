edcc2f3e1fac127346c342754dc7b4b70a010928
Kona1956 <38185987+K-ona@users.noreply.github.com>
2021-07-15 19:47:09 +0800

    .

--- LeetCode/100/1808.cpp
+++ LeetCode/100/1808.cpp
@@ -7,6 +7,13 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+class Solution {
+public:
+    int maxNiceDivisors(int primeFactors) {
+        return primeFactors / 2 * (primeFactors - primeFactors / 2) % ll(1e9 + 7); 
+    }
+};
+
 int main() {
     std::ios_base::sync_with_stdio(false); 
     #ifdef LOCAL_TEST
