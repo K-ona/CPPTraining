dc456e2b125668f3b4362eeb5039f47246924e05
Kona1956 <38185987+K-ona@users.noreply.github.com>
2021-09-18 17:35:08 +0800

    update

--- snippets/Data_Structure/segtree.h
+++ snippets/Data_Structure/segtree.h
@@ -1,11 +1,14 @@
 // created by Kona @VSCode
 #include "../../../include/utility.h"
-namespace KonaImpl {
 
 // 区间更新区间查询线段树模板（区间长度为1即为单点查询）
 // 模板题：http://poj.org/problem?id=3468、https://www.luogu.com.cn/record/9549488
-namespace SectionSumSegTree {
+namespace KonaImpl {
 
+#define lc(x) (x << 1)
+#define rc(x) (x << 1 | 1)
+
+namespace SectionSumSegTree {
 // 设为实际节点数的4倍
 template<typename Sz_T, typename Val_T>
 struct node
@@ -17,17 +20,17 @@ struct node
 // 通过子区间计算该区间值
 template<typename Sz_T, typename Val_T>
 inline void push_up(Sz_T rt, node<Sz_T, Val_T> T[]) {
-  T[rt].w = T[rt + rt].w + T[rt + rt + 1].w; 
+  T[rt].w = T[lc(rt)].w + T[rc(rt)].w; 
 }
 
 // 将当前lazy标记，下压传递给左右子区间
 template<typename Sz_T, typename Val_T>
 inline void push_down(Sz_T rt, node<Sz_T, Val_T> T[]) {
   if (T[rt].laz) {
-    T[rt + rt].w += T[rt + rt].siz * T[rt].laz; 
-    T[rt + rt].laz += T[rt].laz; 
-    T[rt + rt + 1].w += T[rt + rt + 1].siz * T[rt].laz; 
-    T[rt + rt + 1].laz += T[rt].laz; 
+    T[lc(rt)].w += T[lc(rt)].siz * T[rt].laz; 
+    T[lc(rt)].laz += T[rt].laz; 
+    T[rc(rt)].w += T[rc(rt)].siz * T[rt].laz; 
+    T[rc(rt)].laz += T[rt].laz; 
     T[rt].laz = static_cast<Val_T>(0); // 取消当前区间标记
   }
 }
@@ -40,9 +43,9 @@ inline void build(Sz_T rt, Sz_T l, Sz_T r, node<Sz_T, Val_T> T[], Sz_T val[]) {
     T[rt].w = val[l]; 
   }
   else {
-    Sz_T mid = (L + R) / static_cast<Sz_T>(2); 
-    build(rt + rt, l, mid, T, val); 
-    build(rt + rt + 1, mid + 1, r, T, val); 
+    Sz_T mid = (l + r) / static_cast<Sz_T>(2); 
+    build(lc(rt), l, mid, T, val); 
+    build(rc(rt), mid + 1, r, T, val); 
     push_up(rt, T); 
   }
 }
@@ -58,9 +61,9 @@ inline void add(Sz_T rt, Sz_T l, Sz_T r, Val_T x, node<Sz_T, Val_T> T[]) {
   }
   push_down(rt, T); 
   Sz_T mid = (L + R) / static_cast<Sz_T>(2); 
-  if (l <= mid) add(rt + rt, l, r, x, T);
-  if (mid < r) add(rt + rt + 1, l, r, x, T);
-  push_up(rt); 
+  if (l <= mid) add(lc(rt), l, r, x, T);
+  if (mid < r) add(rc(rt), l, r, x, T);
+  push_up(rt, T); 
 }
 
 // 区间求和
