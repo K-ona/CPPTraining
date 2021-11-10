3c00395f6d6e8035492ecf5bc52fb63898ac043d
Kona1956 <38185987+K-ona@users.noreply.github.com>
2021-11-10 17:26:21 +0800

    update

---
+++ snippets/algorithm/DP/state_compression/Mondriaan_dream.cpp
@@ -0,0 +1,28 @@
+// created by Kona @VSCode
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <queue>
+#include <vector>
+
+#define LOCAL_TEST
+typedef long long ll;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::queue;
+using std::string;
+using std::vector;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+#ifdef LOCAL_TEST
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  /* code */
+
+  return 0;
+}
