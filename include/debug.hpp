32bd8ee81954f0344ca316066bd53c74fe7b98f9
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-03-27 15:40:46 +0800

    first debug

---
+++ include/debug.hpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+#ifdef DEBUG_MODE
+void DEBUG(std::string s) {
+  std::cout << s << std::endl;
+}
+#endif
+
+#ifndef DEBUG_MODE
+void DEBUG(std::string s) {
+}
+#endif
