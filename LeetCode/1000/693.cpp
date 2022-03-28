70caa88429280e9c999826ee31108fe5da4f37d2
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-03-28 15:41:59 +0800

    水

---
+++ LeetCode/1000/693.cpp
@@ -0,0 +1,43 @@
+// created by Kona @VSCode
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <string.h>
+
+#define LOCAL_TEST
+typedef long long ll;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::queue;
+using std::string;
+using std::vector;
+
+class Solution {
+ public:
+  bool hasAlternatingBits(int n) {
+    int x = n & 1;
+    while (n) {
+      if (x != (n & 1)) return false;
+      n >>= 1;
+      x ^= 1;
+    }
+    return true;
+  }
+};
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+#ifdef LOCAL_TEST
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  /* code */
+
+  return 0;
+}
