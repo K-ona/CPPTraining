7854243f6e18731a7c795e2dd63ebb86088d4640
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-04-06 16:38:01 +0800

    update

--- LeetCode/100/40.cpp
+++ LeetCode/100/40.cpp
@@ -19,6 +19,7 @@ using std::string;
 using std::vector;
 
 int stk[105]; 
+int presum[105]; 
 int top; 
 class Solution {
   int n;
@@ -27,6 +28,10 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
     std::sort(candidates.begin(), candidates.end()); 
     n = candidates.size(); 
+    presum[0] = candidates[0]; 
+    for (int i = 1; i < n; ++i) {
+      presum[i] = presum[i - 1] + candidates[i]; 
+    }
     DFS(candidates, 0, target, 0); 
     std::sort(res.begin(), res.end()); 
     res.resize(std::unique(res.begin(), res.end()) - res.begin()); 
@@ -34,13 +39,17 @@ public:
   }
 
   void DFS(vector<int>& candidates, int pos, int target, int top) {
-    // cout << pos << endl; 
     if (!target) {
       res.push_back(vector<int>(stk, stk + top)); 
       return ;
     }
     for (int i = pos; i < n; ++i) {
-      if (target - candidates[i] >= 0) {
+      int cnt = target / candidates[i]; 
+    //   cout << i << endl; 
+      while (i + cnt < n and candidates[i + cnt] == candidates[i]) { ++i; }
+    //   --i; 
+
+      if (i < n and target - candidates[i] >= 0 and presum[n - 1] - presum[i] >= target - candidates[i]) {
         // cout << i << " " << target << endl; 
         stk[top] = candidates[i]; 
         DFS(candidates, i + 1, target - candidates[i], top + 1); 
