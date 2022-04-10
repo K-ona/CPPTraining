1528a49cfce3a71d2cfad06b8f2b7199dc7436d6
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-04-10 13:47:36 +0800

    水“

---
+++ LeetCode/100/83.cpp
@@ -0,0 +1,55 @@
+// created by Kona @VSCode
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <queue>
+#include <vector>
+
+#include <string.h>
+
+#define LOCAL_TEST
+typedef long long ll;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::queue;
+using std::string;
+using std::vector;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* deleteDuplicates(ListNode* head) {
+    auto ans = head; 
+    while (head) {
+      while (head->next and head->next->val == head->val) {
+        head->next = head->next->next;
+      } 
+      head = head->next;
+    }
+    return ans; 
+  }
+};
+
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+#ifdef LOCAL_TEST
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  /* code */
+
+  return 0;
+}
---
+++ LeetCode/1000/804.cpp
@@ -0,0 +1,48 @@
+// created by Kona @VSCode
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+
+#include <string.h>
+
+#define LOCAL_TEST
+typedef long long ll;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::queue;
+using std::string;
+using std::vector;
+
+string arr[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+class Solution {
+ public:
+  int uniqueMorseRepresentations(vector<string>& words) {
+    std::unordered_map<string, int> M; 
+    for (auto &word: words) {
+      string morse = "";
+      for (auto c: word) {
+        morse += arr[c - 'a'];
+      }
+      ++M[morse];      
+    }
+    return M.size();
+  }
+};
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+#ifdef LOCAL_TEST
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  /* code */
+
+  return 0;
+}
