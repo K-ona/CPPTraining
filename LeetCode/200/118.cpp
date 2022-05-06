073c6477d8e5a7f8e5ffafb189574be16a2fdfd5
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-05-06 15:06:18 +0800

    水

---
+++ LeetCode/200/118.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+      vector<vector<int>> ans(numRows);
+      ans[0] = {1}; 
+      for (int i = 1; i < numRows; ++i) {
+        int sz = ans[i - 1].size();
+        vector<int> tmp;
+        tmp.reserve(sz + 1); 
+        tmp.push_back(1);  
+        for (int j = 0; j + 1 < sz; ++j) {
+          tmp.push_back(ans[i - 1][j] + ans[i - 1][j + 1]);
+        }
+        tmp.push_back(1);  
+        ans[i] = std::move(tmp); 
+      }
+      return ans; 
+    }
+};
