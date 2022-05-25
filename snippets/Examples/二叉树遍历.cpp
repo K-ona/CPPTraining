d0a8299599d4f82ab26abd54f0cb4c828c8ec39b
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-05-25 14:48:38 +0800

    update 后续遍历

--- snippets/Examples/二叉树遍历.cpp
+++ snippets/Examples/二叉树遍历.cpp
@@ -21,14 +21,15 @@ class Solution {
     stk.push(root);
     while (!stk.empty()) {
       auto root = stk.top();
-      stk.pop(); 
+      stk.pop();
       while (root) {
-        if (root->right) stk.push(root->right); 
-        res.push_back(root->val); 
-        root = root->left; 
+        if (root->right)
+          stk.push(root->right);
+        res.push_back(root->val);
+        root = root->left;
       }
     }
-    return res; 
+    return res;
   }
 };
 
@@ -37,18 +38,19 @@ class Solution {
  public:
   std::vector<int> inorderTraversal(TreeNode* root) {
     std::stack<TreeNode*> stk;
-    std::vector<int> res; 
+    std::vector<int> res;
     res.reserve(100);
-    
+
     while (root or !stk.empty()) {
       if (root) {
-        stk.push(root); 
-        root = root->left; 
+        stk.push(root);
+        root = root->left;
       } else {
-        root = stk.top(); stk.pop(); 
-        res.push_back(root->val); 
+        root = stk.top();
+        stk.pop();
+        res.push_back(root->val);
         root = root->right;
-      } 
+      }
     }
     return res;
   }
@@ -57,12 +59,34 @@ class Solution {
 // 后序遍历
 
 class Solution {
-public:
-    std::vector<int> postorderTraversal(TreeNode* root) {
-        std::vector<int> res;
-        res.reserve(100); 
-
-
-        return res; 
+  void find(std::stack<TreeNode*>& stk) {
+    while (auto cur = stk.top()) {
+      if (cur->left) {
+        if (cur->right) stk.push(cur->right); 
+        stk.push(cur->left); 
+      } else {
+        stk.push(cur->right); 
+      }
     }
-}; 
+    stk.pop(); // 栈顶为空时 while 循环结束，弹出空节点
+  }
+
+ public:
+  std::vector<int> postorderTraversal(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    std::vector<int> res;
+    res.reserve(100);
+
+    if (root) stk.push(root); 
+    while (!stk.empty()) {
+      if (stk.size() and ((stk.top()->left == root and !stk.top()->right) or (stk.top()->right == root))) {
+      } else {
+        find(stk); 
+      }
+      root = stk.top(); 
+      stk.pop(); 
+      res.push_back(root->val); 
+    }
+    return res;
+  }
+};
