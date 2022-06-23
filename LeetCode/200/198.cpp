72f7aea00d596d1e06e652c93db9ea62fe5bff2f
Kona1956 <38185987+K-ona@users.noreply.github.com>
2022-06-23 11:14:39 +0800

    update

--- LeetCode/200/198.cpp
+++ LeetCode/200/198.cpp
@@ -1,15 +1,22 @@
-int dp[105][2];
-
 class Solution {
  public:
-  int rob(vector<int>& nums) {
-    int n = nums.size();
-    dp[0][0] = 0;
-    dp[0][1] = nums[0];
-    for (int i = 1; i < n; ++i) {
-      dp[i][0] = std::max(dp[i - 1][0], dp[i - 1][1]);
-      dp[i][1] = dp[i - 1][0] + nums[i];
+  int robRange(vector<int>& nums, int start, int end) {
+    int first = nums[start], second = max(nums[start], nums[start + 1]);
+    for (int i = start + 2; i <= end; i++) {
+      int temp = second;
+      second = max(first + nums[i], second);
+      first = temp;
     }
-    return std::max(dp[n - 1][0], dp[n - 1][1]);
+    return second;
+  }
+
+  int rob(vector<int>& nums) {
+    int length = nums.size();
+    if (length == 1) {
+      return nums[0];
+    } else if (length == 2) {
+      return max(nums[0], nums[1]);
+    }
+    return max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
   }
 };
